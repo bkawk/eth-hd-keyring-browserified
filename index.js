0ac97f658599ad5c6464fcb9433aa8cbfb55a285
@bkawk <email.will.in.china@gmail.com>
2017-04-06 15:49:10 +0800

    babelify

---
+++ index.js
@@ -0,0 +1,5 @@
+var hdkeyring = require('eth-hd-keyring')
+
+
+//browserify index.js -t [ babelify --presets es2015 ] | uglifyjs > bundle.js
+//browserify index.js -t [ babelify --presets es2015 ]  > bundle.js
